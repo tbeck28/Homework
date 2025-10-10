6e59d71b983cfa99ca91a8d3a803b35dd5a4a831
Troy Beck <beginnerdev28@gmail.com>
2025-10-10 11:36:11 -0500

    completed part 2 of my week 2 homework

---
+++ week2/homeworkweek2.js
@@ -0,0 +1,15 @@
+
+
+let pizzaPlace = 'The Pizzaria';
+let numberOfToppings = 5;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+let message = `Welcome to my place, ${pizzaPlace}, how many toppings do you like on your pizza!? I usually have about ${numberOfToppings}`;
+
+if (numberOfToppings >= 10) {
+    console.log('Quality, not quantity');
+} else {
+    console.log('A whole log of pizza');
+}
