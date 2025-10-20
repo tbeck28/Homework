24db2dca116ef834535321350d575f5e3b95decf
Troy Beck <beginnerdev28@gmail.com>
2025-10-20 15:00:20 -0500

    made the changes however i am not sure why but i am now facing two issues? one, the array that is printing is not separated with spaces anymore and on the last line i am getting part of the object in the final statement? what am i doing wrong, ive spent what feels like most of my morning trying to troubleshoot this issue and i am constantly coming up empty handed

--- week3/week3Homework.js
+++ week3/week3Homework.js
@@ -4,56 +4,52 @@ let pizzaTopping = ['tomatoes', 'onions', 'olives', 'steak']
 
 // 2
 function greetCustomer () {
-    let greeting = "Hello and welcome to Your mom's Pizzeria, here are our toppings: ";
+    let greeting = "Hello and welcome to Your mom's Pizzeria, here are our toppings: "
     for (let x = 0; x < pizzaTopping.length; x++) {
-        let top = pizzaTopping[x]
+        greeting += pizzaTopping[x]
     }
-    console.log(greeting + pizzaTopping)
+    console.log(greeting)
 }
-
-
 
 // 3
 function getPizzaOrder (size, crust, ...toppings) {
     let order = `So you want a ${size} pizza with ${crust} crust, and ${toppings}, I'll get that started for you! `
-    const newArray = [size, crust, ...toppings]
     console.log(order)
-    return newArray
+    return [size, crust, toppings]
+    }
+
+
+// 4
+
+function getPizzaOrder(size, crust, ...toppings) {
+    console.log(`One ${size} pizza with ${crust} crust and ${toppings}`)
+    let array2 = [size, crust, toppings];
+    return array2;
 }
 
 
-let y
-
-// 4
-function preparePizza ([size, crust, toppings]) {
-        y = {
+function preparePizza([size, crust, ...toppings]) {
+    console.log('cooking pizza!')
+    return {
         size: size,
         crust: crust,
         toppings: toppings
+        }
+}
+
+
+
+function servePizza(x) {
+    let order = `the ${x.size} pizza with ${x.crust} and ${x.toppings} is done`
+    for(let topping of x.toppings) {
+        order += `${topping}`;
     }
-    console.log(`Cooking the pizza!`)
-    return y
+    console.log(`${order}`)
+    return x;
 }
 
 
-
-// 5 
-
-let pixxa = {
-    size: 'large',
-    crust: 'thin',
-    toppings: 'olives',
-}
-
-
-let pizza;
-let message;
-function servePizza (obj) {
-    console.log(`The ${obj.size} pizza with ${obj.crust} crust and ${obj.toppings} is done `)
-}
-
-
-greetCustomer('Quinn')
-getPizzaOrder('large', 'thin', 'tomatoes')
-preparePizza(['large', 'thin', 'olives'])
-servePizza(y)
+greetCustomer();
+let order = getPizzaOrder('large', 'thin', 'tomates');
+let pizza = preparePizza(order);
+servePizza(pizza);
